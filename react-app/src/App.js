25c3cb3fa04fad6f1fe713d6abbcbc79e339e9bb
Dillon <dfcross18@gmail.com>
2024-04-15 01:43:47 -0700

    setup api

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
 function App() {
+  fetch('http://127.0.0.1:8000/api/')
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.error("Error fetching data: ", error)
+  })
   return (
     <>
       <div>
